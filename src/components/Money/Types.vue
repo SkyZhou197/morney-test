e37e3275219631b841bfadae59bf30fc71e7ee45
skyzhou <zhouhaomin019@gmail.com>
2020-02-07 15:33:02 +0800

    收集四个组件的数据到 record

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -2,25 +2,24 @@ import Types from '@/components/Money/Types.vue';
 <template>
   <div>
     <ul class="types">
-      <li :class="type ==='-' && 'selected'" @click="selectType('-')">支出</li>
-      <li :class="type ==='+' && 'selected'" @click="selectType('+')">收入</li>
+      <li :class="value ==='-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="value ==='+' && 'selected'" @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { Component } from "vue-property-decorator";
+import { Component, Prop, Watch } from "vue-property-decorator";
 
 @Component
 export default class Types extends Vue {
-  type = "-"; //"-"表示支出,"+"表示收入
+  @Prop() readonly value!: string;
   selectType(type: string) {
     if (type !== "-" && type !== "+") {
-      //type只能是"-"和"+"中一个
       throw new Error("type is unknown");
     }
-    this.type = type;
+    this.$emit("update:value", type);
   }
 }
 </script>

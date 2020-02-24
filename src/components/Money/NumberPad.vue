2dc70bc235f27270705fe9cd8e3247fc5d33da9b
skyzhou <zhouhaomin019@gmail.com>
2020-02-24 10:57:01 +0800

    完成统计页面

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="numberPad">
-    <div class="output">{{output}}</div>
+    <div class="output">{{ output }}</div>
     <div class="buttons">
       <button @click="inputContent">1</button>
       <button @click="inputContent">2</button>
@@ -26,7 +26,7 @@ import { Component, Prop } from "vue-property-decorator";
 
 @Component
 export default class NumberPad extends Vue {
-  @Prop() readonly value!: number;
+  @Prop(Number) readonly value!: number;
   output = this.value.toString();
 
   inputContent(event: MouseEvent) {
@@ -59,8 +59,9 @@ export default class NumberPad extends Vue {
     this.output = "0";
   }
   ok() {
-    this.$emit("update:value", this.output);
-    this.$emit("submit", this.output);
+    const number = parseFloat(this.output);
+    this.$emit("update:value", number);
+    this.$emit("submit", number);
     this.output = "0";
   }
 }
